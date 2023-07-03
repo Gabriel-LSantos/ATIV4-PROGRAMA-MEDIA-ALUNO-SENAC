45931c39d817d9db6cfc177d3afaf7ec2bec1fdb
gabri <gabri@DESKTOP-I4U4N50>
2023-07-02 22:43:44 -0300

    Adicionar arquivos de projeto.

---
+++ PROGRAMA-MEDIA-ALUNO-SENAC/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double n1, n2, media;
+
+        Console.WriteLine("Qual sua nota no 1º semestre?");
+        n1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Qual sua nota no 2º semestre?");
+        n2 = double.Parse(Console.ReadLine());
+
+        media = (n1 + n2) / 2;
+        Console.WriteLine($"A média é {media}");
+
+    }
+}
